f7a9856cbd3bd3ca6fc9155fd28a7e8eef7a6d1c
Somaia Ahmed <52898207+somaiaahmed@users.noreply.github.com>
2023-09-03 04:32:21 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+
+
+    double x,y;
+    char opertion;
+
+    cout<<"SIMPLE CALCULATOR! \n";
+
+    cout<<"Enter The First Number: ";
+    cin>>x;
+
+    cout<<"Enter The Second Number: ";
+    cin>>y;
+
+    cout<<"Choose an operation (+, -, *, /):";
+    cin>>opertion;
+
+    double res=0;
+
+    if(opertion=='+')
+       res=x+y;
+    else  if(opertion=='-')
+        res=x-y;
+    else  if(opertion=='*')
+        res=x*y;
+    else  if(opertion=='/') {
+        if(y==0){
+            cout<<"Error! Division By zero.\n";
+            return 0;
+        }
+        res = x / y;
+    }
+    else {
+        cout << "Operation doesn't exist, Please choose a valid operation\n";
+    return 0;
+    }
+    cout<<"Result = "<<res<<endl;
+}
