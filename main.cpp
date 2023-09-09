3c2cca4bb5595a7799af8caf935e47c2438f5e72
Somaia Ahmed <52898207+somaiaahmed@users.noreply.github.com>
2023-09-09 04:15:07 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,160 +1,56 @@
 #include <bits/stdc++.h>
+#define ll long long
 using namespace std;
 
+const int N = INT_MAX;
 
-char board[3][3];
-char player1;
-char player2;
-
-void resetBoard(){
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            board[i][j]=' ';
-
-        }
-    }
-}
-void printBoard(){
-
-    cout<<" "<<board[0][0]<<" | "<<board[0][1]<<" | "<<board[0][2]<<" ";
-    cout<<"\n"<<"---"<<"|"<<"---"<<"|"<<"---"<<"\n";
-    cout<<" "<<board[1][0]<<" | "<<board[1][1]<<" | "<<board[1][2]<<" ";
-    cout<<"\n"<<"---"<<"|"<<"---"<<"|"<<"---"<<"\n";
-    cout<<" "<<board[2][0]<<" | "<<board[2][1]<<" | "<<board[2][2]<<" ";
-  //  cout<<"\n"<<"---"<<"|"<<"---"<<"|"<<"---"<<"\n";
-}
-int checkFreeSpace(){
-int freeSpace=9;
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            if(board[i][j]!=' ')
-                freeSpace--;
-
-        }
-    }
-    return freeSpace;
-}
-void player1Move(char c){
-    int x,y;
-do{
-    cout<<"\n"<<"Player "<<c<<" Enter row #(1-3):";
-
-    cin>>x;
-    x--;
-    cout<<"\n"<<"Player "<<c<<" Enter column #(1-3):";
-    cin>>y;
-    y--;
-    if(board[x][y]!=' ')
-        cout<<"\n"<<"Invalid move!\n";
-    else {
-        board[x][y] = c;
-        break;
-    }
-}
-    while (board[x][y]!=' ');
-
-}
-void player2Move(char c){
-    int x,y;
-    do{
-        cout<<endl<<"Player "<<c<<" Enter row #(1-3):";
-        cin>>x;
-        x--;
-        cout<<endl<<"Player "<<c<<" Enter column #(1-3):";
-        cin>>y;
-        y--;
-        if(board[x][y]!=' ')
-            cout<<endl<<"Invalid move!\n";
-        else {
-            board[x][y] = c;
-            break;
-        }
-    }
-    while (board[x][y]!=' ');
-}
-char checkWinner(){
-    //check rows
-    for(int i=0;i<3;i++) {
-        if (board[i][0] == board[i][1] && board[i][0] == board[i][2]) {
-        return board[i][0];
-
-     }
-    }
-    //check columns
-    for(int i=0;i<3;i++) {
-        if (board[0][i] == board[1][i] && board[0][i] == board[2][i]) {
-            return board[0][i];
-
-        }
-    }
-    //check diagonals
-        if (board[0][0] == board[1][1] && board[0][0] == board[2][2]) {
-            return board[0][0];
-
-        }
-    //check diagonals
-    if (board[0][2] == board[1][1] && board[0][2] == board[2][0]) {
-        return board[0][2];
-
-    }
-    return ' ';
-
-
-}
-void printWinner(char winner){
-    if(winner == player1){
-        cout<<endl<<"PLAYER ONE WINS!\n";
-    }
-    else if(winner == player2){
-        cout<<endl<<"PLAYER TWO WINS!\n";
-    }
-    else
-        cout<<endl<<"IT'S A TIE!\n";
-
-
-}
-
-
+#define Fast cin.tie(NULL); cout.tie(NULL); ios_base::sync_with_stdio(false);
 
 int main() {
-    char winner=' ';
-    char response=' ';
 
-       cout<<endl<<"Welcome to tic-tac-toe game!\n";
-
-       //do{
-
-           cout << endl<<"PLAYER ONE IS: ";
-        cin >> player1;
+    //Fast
+    random_device rd;
+    mt19937 mt(rd());
 
 
-  do { cout<<"\n"<<"PLAYER TWO IS: ";
-    cin>>player2;}
-  while(player1==player2);
-cout<<"\n";
+    cout<<"Welcome to the number guessing game! \n";
 
-resetBoard();
+    int min_num=1;
+    int max_num=500;
 
-        while (winner == ' ' && checkFreeSpace() > 0) {
-            printBoard();
+    cout<<"I'm thinking of a number between "<<min_num<<" and "<<max_num<<" . Try to guess it. \n";
 
-            player1Move(player1);
-            printBoard();
-            winner = checkWinner();
-            if (winner != ' ' || checkFreeSpace() == 0)
-                break;
+    uniform_int_distribution<int> distribution(min_num, max_num);
+    int secret_num=distribution(mt);
 
-            player2Move(player2);
-            winner = checkWinner();
+ //   cout<<secret_num<<endl;
+
+    cout<<"Enter the guess: \n";
+    int guessed_num;
+    cin>>guessed_num;
+
+    int attempt=1;
+
+    while(guessed_num!=secret_num){
+
+        attempt++;
+        if(guessed_num<secret_num) {
+
+            if (secret_num - guessed_num >= 50)
+                cout << "The guess is too low! Try again." << endl;
+          else
+              cout << "The guess is low! Try again." << endl;
         }
+        if(guessed_num>secret_num) {
 
-    printBoard();
-    printWinner(winner);
+            if (guessed_num - secret_num >= 50)
+                cout << "The guess is too high! Try again." << endl;
+           else
+               cout << "The guess is high! Try again." << endl;
+        }
+        cin>>guessed_num;
 
-   /* cout<<endl<<"Would you like to play again? (Y/N):";
-    cin>>response;
-}while(response=='Y');*/
-    cout<<endl<<"Thank you for playing!\n";
-    return 0;
+    }
+
+    cout<<"Congratulations! You guessed the number "<<secret_num<<" in "<<attempt<<" attempts! \n";
 }
-
