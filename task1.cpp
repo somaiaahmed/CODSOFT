98b60d6e9ee6b0eab9eb1b238714f1bb811c7648
Somaia Ahmed <52898207+somaiaahmed@users.noreply.github.com>
2023-09-24 02:31:03 +0300

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -1,14 +1,11 @@
 #include <bits/stdc++.h>
-#define ll long long
+
 using namespace std;
 
-const int N = INT_MAX;
-
-#define Fast cin.tie(NULL); cout.tie(NULL); ios_base::sync_with_stdio(false);
 
 int main() {
 
-    //Fast
+
     random_device rd;
     mt19937 mt(rd());
 
